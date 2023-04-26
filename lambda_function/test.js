11c961e98bbb5c4509fb953b611dc8fb18719439
John Morahan <john@invotra.co>
2023-04-26 15:42:14 +0100

    tidy up test.js

--- lambda_function/test.js
+++ lambda_function/test.js
@@ -1,17 +1,17 @@
 
-exports.handler = async function (event, context, callback) {
+exports.handler = async function (event, context) {
 
-  const hello_world = { "hello": "world"};
+  const hello_world = { "hello": "world" };
   console.log(JSON.stringify(hello_world));
 
-  let response = {
+  const response = {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'OPTIONS,POST',
       'Content-Type': 'text/html',
     },
-  
+
     body: JSON.stringify(hello_world),
   };
 
